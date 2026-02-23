91978da84d56aace87dd6e6a8559fa07823f0c7d
yashiikaaaa <yashika@truestate.in>
2026-02-23 12:48:36 +0530

    homes image

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,4 +1,4 @@
-import herobanner from "../assets/home/prestigeraintreehome.jpeg"; // Importing hero banner background image
+import herobanner from "../assets/sobha hoskote.jpeg"; // Importing hero banner background image
 import Button from "../components/button/buttonMain"; // Importing the Button component for reusability
 import homeLocation from "../assets/home/location.svg"; // Importing location icon
 import { useEffect, useState } from "react";
